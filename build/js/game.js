256994039a94e76d91767834b4270dcb5a99cae8
Adam Faur <adam.faur@gmail.com>
2017-01-29 14:30:49 -0800

    Add buttons for gameplay. Add fish movement. Add click to move fishNet.

--- build/js/game.js
+++ build/js/game.js
@@ -15,6 +15,81 @@ class EventEmitter {
   }
 }
 
+class Stage {
+  constructor(gameObjects) {
+    this.game = gameObjects
+    this.started = false
+    this.fishNetMovingUp = false
+    this.fishInterval = null
+    this.fishNetInterval = null
+    this.fishNetVelocity = 0.02
+    this.pageBody = document.getElementById("body")
+    this.playButton = document.getElementById("playButton")
+    this.endButton = document.getElementById("endButton")
+    this.pageBody.onclick = (evt) => {
+      if ( evt.target.id === 'playButton' || evt.target.id === 'endButton' ) {
+      } else {
+        if ( this.started === true && this.fishNetMovingUp === false ) {
+          this.pushUpNet()
+        }
+      }
+    }
+    this.playButton.onclick = (evt) => {
+      evt.preventDefault()
+      this.play()
+    }
+    this.endButton.onclick = (evt) => {
+      evt.preventDefault()
+      this.end()
+    }
+    this.game.fishNet.set(0)
+  }
+  play() {
+    if (this.fishInterval) { clearInterval(this.fishInterval) }
+    if (this.fishNetInterval) { clearInterval(this.fishNetInterval) }
+    this.fishInterval = setInterval(() => {
+      let random = Math.random()
+      this.game.fish.set(random)
+    }, 2000)
+    this.fishNetInterval = setInterval(() => {
+      if ( this.fishNetMovingUp === false ) {
+        let current = this.game.fishNet.get()
+        let velocity = this.fishNetVelocity
+        if ( (current - velocity) > 0 ) {
+          this.game.fishNet.set(current - velocity)
+        } else {
+          this.game.fishNet.set(0)
+        }
+      }
+    }, 100)
+    this.started = true
+  }
+  end() {
+    clearInterval(this.fishInterval)
+    clearInterval(this.fishNetInterval)
+    this.game.fish.set(0)
+    this.game.fishNet.set(0)
+    this.started = false
+  }
+  pushUpNet() {
+    if ( this.fishNetMovingUp === false ) {
+      if ( (this.game.fishNet.get() + 0.05) < 1 ) {
+        this.game.fishNet.set( this.game.fishNet.get() + 0.05 )
+        this.fishNetMovingUp = true
+        setTimeout(() => {
+          this.fishNetMovingUp = false
+        }, 100)
+      } else {
+        this.game.fishNet.set(1)
+      }
+      this.fishNetMovingUp = true
+      setTimeout(() => {
+        this.fishNetMovingUp = false
+      }, 100)
+    }
+  }
+}
+
 class Reel {
   constructor(el, options = {}) {
     this.el = el
@@ -87,9 +162,13 @@ class FishNet {
     this.set(options.value || 0)
   }
   set (value) {
+    this.value = value
     const revValue = Math.abs(value - 1)
-    this.value = ((this.floor - this.ceil) * revValue) + this.ceil
-    this.el.style.top = this.value + 'px'
+    this.position = ((this.floor - this.ceil) * revValue) + this.ceil
+    this.el.style.top = this.position + 'px'
+  }
+  get () {
+    return this.value
   }
 }
 
@@ -97,3 +176,7 @@ const fish = new Fish(document.querySelector('.ic-fish'), {})
 const fishNet = new FishNet(document.querySelector('.ic-fishnet'), {})
 const progress = new Progress(document.querySelector('.progress'), {})
 const reel = new Reel(document.querySelector('.reel-wrapper'), {})
+
+const stage = new Stage({
+  fish, fishNet, progress, reel
+})
